6420f37e9483a7bae3852988e6f338d3c279c1c7
noaione <noaione0809@gmail.com>
2021-08-14 09:23:55 +0700

    Add twitter post mirror

---
+++ cogs/feeds/twitter.py
@@ -0,0 +1,64 @@
+import logging
+from typing import List
+import aiohttp
+
+import discord
+from discord.ext import commands, tasks
+from phelper.bot import PotiaBot
+
+
+class FeedsTwitterPosts(commands.Cog):
+    ENDPOINT = "https://api.twitter.com/2/users/1385480130068246530/tweets"
+
+    def __init__(self, bot: PotiaBot) -> None:
+        self.bot = bot
+
+        self.logger = logging.getLogger("Feeds.TwitterPosts")
+        self._news_channels: discord.TextChannel = self.bot.get_channel(864043313166155797)
+        self._twitter_posts.start()
+
+    def cog_unload(self):
+        self._twitter_posts.cancel()
+
+    async def _fetch_twitter_posts(self):
+        headers = {"Authorization": f"Bearer {self.bot.bot_config['twitter']}", "User-Agent": "PotiaBot/1.0"}
+        params = {
+            "expansions": "author_id",
+            "tweet.fields": "created_at",
+        }
+        async with aiohttp.ClientSession(headers=headers) as session:
+            async with session.get(self.ENDPOINT, params=params) as resp:
+                data = await resp.json()
+                return data.get("data")
+
+    @tasks.loop(minutes=3.0)
+    async def _twitter_posts(self):
+        try:
+            self.logger.info("Starting _twitter_posts process...")
+            collected_posts = await self._fetch_twitter_posts()
+            old_posts_data: List[str] = await self.bot.redis.get("potiamuse_twposts", [])
+            not_sended_yet = []
+            for post in collected_posts:
+                if post["id"] not in old_posts_data:
+                    not_sended_yet.append(post["id"])
+
+            message_fmt = "**Postingan baru di Twitter Muse Indonesia!**\n"
+            message_fmt += "Link: <https://twitter.com/muse_indonesia/status/{id}>"
+            for post in not_sended_yet:
+                try:
+                    messages: discord.Message = await self._news_channels.send(
+                        content=message_fmt.format(id=post)
+                    )
+                    old_posts_data.append(post)
+                except (discord.Forbidden, discord.HTTPException):
+                    self.logger.warning(f"Failed to send this post: {post['id']}")
+                    continue
+                try:
+                    await messages.publish()
+                except (discord.Forbidden, discord.HTTPException):
+                    self.logger.warning(f"Failed to publish post: {post['id']}, ignoring...")
+            self.logger.info("Saving posted data to redis...")
+            await self.bot.redis.set("potiamuse_twposts", old_posts_data)
+        except Exception as e:
+            self.logger.error("Failed to run `_twitter_posts`, traceback and stuff:")
+            self.bot.echo_error(e)
