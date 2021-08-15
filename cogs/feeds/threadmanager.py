29310a57922fad81118443d6045b8e04d7a653de
noaione <noaione0809@gmail.com>
2021-08-15 18:29:39 +0700

    threadmanager: fix thread checking etc

--- cogs/feeds/threadmanager.py
+++ cogs/feeds/threadmanager.py
@@ -12,7 +12,7 @@ class FeedsThreadManager(commands.Cog):
         self.logger = logging.getLogger("Feeds.ThreadManager")
         self._TEMPLATE = "🔴 | {title} | {id}"
         self._MSG_TEMPLATE = (
-            "Thread ini akan digunakan untuk membicarakan `{title}` dengan\nhttps://youtube.com/watch?v={id}"
+            "Thread ini akan digunakan untuk membicarakan `{title}`\nhttps://youtube.com/watch?v={id}"
         )
 
         self._EVENTS = {
@@ -26,8 +26,25 @@ class FeedsThreadManager(commands.Cog):
         for event in self._EVENTS.keys():
             self.bot.pevents.off(event)
 
+    @staticmethod
+    def _remove_takarir(title: str):
+        return title.replace("[Takarir Indonesia]", "").rstrip()
+
+    def _cleanup_title(self, id: str, title: str):
+        id_len = len(id)
+        first_part = "🔴 | "
+        second_part = " | "
+        additional = " [...]"
+        title = self._remove_takarir(title)
+        MAX_LEN = 100
+        if len(title) + id_len + len(first_part) + len(second_part) > MAX_LEN:
+            total_cut = id_len + len(first_part) + len(second_part) + len(additional)
+            title = title[: MAX_LEN - total_cut] + additional
+        return title
+
     async def _on_new_live_creation(self, data: dict):
-        channel: discord.TextChannel = self.bot.get_channel(864019381230501898)
+        self.logger.info("Received event for live thread creation.")
+        channel: discord.TextChannel = self.bot.get_channel(864019283490242570)
         live_id = data["id"]
         title = data["title"]
         self.logger.info("Checking if live thread exists...")
@@ -37,16 +54,25 @@ class FeedsThreadManager(commands.Cog):
             return
 
         self.logger.info("Live thread does not exist, creating...")
-        new_thread = await channel.start_thread(
-            name=self._TEMPLATE.format(id=live_id, title=title),
+        new_thread = await channel.create_thread(
+            name=self._TEMPLATE.format(id=live_id, title=self._cleanup_title(live_id, title)),
             type=ChannelType.public_thread,
-            reason="Live thread",
+            reason=f"Auto creation for live thread: {live_id}",
         )
-        await new_thread.send(content=self._MSG_TEMPLATE.format(id=live_id, title=title))
+        self.logger.info("Sending sample message...")
+        message = await new_thread.send(
+            content=self._MSG_TEMPLATE.format(id=live_id, title=self._remove_takarir(title))
+        )
+        self.logger.info("Saving to redis state!")
         await self.bot.redis.set(f"potia_livethread_{live_id}", new_thread.id)
+        try:
+            await message.pin()
+        except discord.Forbidden:
+            pass
 
     async def _on_old_live_archival(self, data: dict):
-        channel: discord.TextChannel = self.bot.get_channel(864019381230501898)
+        self.logger.info("Received event for live thread deletion/archival!")
+        channel: discord.TextChannel = self.bot.get_channel(864019283490242570)
         live_id = data["id"]
         self.logger.info("Checking if live thread exists...")
         exist = await self.bot.redis.get(f"potia_livethread_{live_id}")
