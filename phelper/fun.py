aa7d93e1f935d9237c13c956d719f14dc8f60337
noaione <noaione0809@gmail.com>
2021-09-28 15:22:15 +0800

    yo

---
+++ phelper/fun.py
@@ -0,0 +1,18 @@
+from discord.ext import commands
+from phelper.bot import PotiaBot
+
+
+class PotiaFunStuff(commands.Cog):
+    def __init__(self, bot: PotiaBot):
+        self.bot = bot
+
+    @commands.command(aliases=["tanyaustaz"])
+    async def tanyaustad(self, ctx: commands.Context, *, pertanyaan: str):
+        if not pertanyaan:
+            return await ctx.send("Mohon berikan pertanyaan untuk pak Ustad")
+
+        await ctx.send(f"Pak ustad <@579438382162378752> mau nanya:\n{pertanyaan}")
+
+
+def setup(bot: PotiaBot):
+    bot.add_cog(PotiaFunStuff(bot))
